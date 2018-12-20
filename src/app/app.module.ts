f42de681bcff4aa1d5ed211a9e894f56e6041042
Puneeth Saladi <puneethsaladi@gmail.com>
2018-12-20 17:45:42 +0530

    built basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ParticlesModule } from 'angular-particle';
 import { AppComponent } from './app.component';
+import { MainContentComponent } from './content/mainContent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/content/mainContent.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mainContent',
+  templateUrl: './mainContent.component.html',
+  styleUrls: ['./mainContent.component.css']
+})
+export class MainContentComponent {}
